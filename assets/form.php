705687265892f16818e8d5b7afa0c43359a0e433
slowik <aleksandra.slowik88.gmail.com>
2020-08-26 11:57:04 +0200

    add jumbotron and methode post

--- assets/form.php
+++ assets/form.php
@@ -1,3 +1,16 @@
-<?php
-echo htmlspecialchars($_POST['name']);
+<?php 
+
+    $gender = $firstname = $lastname = $email =$country =$subject = $message = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $firstname = $_POST["firstname"];
+        $lastname = $_POST["lastname"];
+        $gender = $_POST["gender"];
+        $email = $_POST["email"];
+        $country = $_POST["country"];
+        $subject= $_POST["subject"];
+        $message= $_POST["message"];
+    }
+
+
 ?>
