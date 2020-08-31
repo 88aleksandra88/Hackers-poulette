3abd9fd461b53eb08c6dd0eb41a38b7777928982
slowik <aleksandra.slowik88@gmail.com>
2020-08-31 21:12:19 +0200

    semantique/include footer-header

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <link rel="stylesheet" href="assets/style.css">
   <link rel="icon" type="image/png" href="assets/img/hackers-poulette-logo.png" />
@@ -13,28 +12,12 @@
 </head>
 
 <body>
-  <!--Header-------------------------------------------------------------------------------->
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <a class="navbar-brand">Hackers-poulette</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarCollapse">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Shop</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+  <?php
+  include("header.html");
+  ?>
 
-  <!--Jumbo------------------------------------------------------------------------------------------>
+  <!--Jumbo-------------------------------->
+
   <div class="jumbotron jumbotron-fluid">
     <img src="assets/img//hackers-poulette-logo.png" width="250" height="250">
 
@@ -42,24 +25,21 @@
     <p class="lead">Need to get in touch witch the team? We're all ears.</p>
   </div>
   </div>
-  <!---FORM----------------------------->
-  <div>
-<label for="honeypot"></label>
-<input type="text" class="form-control" id="honeypot" placeholder="U can see me?" name="honeypot">
-</div>
 
+  <!---FORM----------------------------->
+
+  <div>
+    <label for="honeypot"></label>
+    <input type="text" class="form-control" id="honeypot" placeholder="U can see me?" name="honeypot">
+  </div>
 
   <form action="recup_donnes.php" method="POST" aria-label="Contact form">
-
     <div class="form-group" text-center">
       <label for="uname">Last name:</label><span style="color: red !important; display: inline; float: none;">*</span>
       <input type="text" class="form-control" id="uname" placeholder="Enter your name" name="name" required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
-<!--Honeypot---------------------------------------------->
-
-
 
     <div class="form-group" text-center">
       <label for="firstname">First name: :</label><span style="color: red ; display: inline; float: none;">*</span>
@@ -73,7 +53,6 @@
       <input type="text" class="form-control" id="gender" name="gender" required>
     </div>
 
-    <!-- Email -->
     <div class="form-group">
       <label for="email">Email:</label><span style="color: red; display: inline; float: none;">*</span>
       <div class="input-group">
@@ -85,17 +64,14 @@
       <span class="error" role="alert" aria-relevant="all"></span>
     </div>
 
-    <!-- Country -->
-
     <div class="form-group">
       <label for="exampleFormControlSelect2">Country:</label><span style="color: red; display: inline; float: none;" required>*</span>
-      <?php 
-             include("countrys.php");
+      <?php
+      include("countrys.php");
       ?>
 
     </div>
 
-    <!-- Subject -->
     <div class="form-group">
       <label for="subject">Subject:</label>
       <select class="form-control" name="subject" id="subject">
@@ -106,67 +82,21 @@
       </select>
     </div>
 
-    <!-- Message -->
     <div class="form-group">
       <label for="message">Message:</label><span style="color: red; display: inline; float: none;">*</span>
       <span class="error" role="alert" aria-relevant="all" required> </span>
       <textarea class="form-control" rows="3" name="message" required></textarea>
     </div>
 
-    <!-- Buttons -->
     <button type="submit" class="btn btn-info" role="button" name="submit">Submit</button>
   </form>
   </div>
   </div>
   </form><br /><br />
   <hr />
-
-  <footer class="container footer footer-expand-md navbar-dark" id="footer">
-    <div class="row">
-      <div class="col-12 col-md">
-        <img src="assets/img/hackers-poulette-logo.png" width="150" height="150" class="d-block mb-2" role="img" viewBox="0 0 24 24" focusable="false">
-        <title>Product</title></img>
-      </div>
-      <div class="col-6 col-md">
-        <h5>Features</h5>
-        <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Cool stuff</a></li>
-          <li><a class="text-muted" href="#">Random feature</a></li>
-          <li><a class="text-muted" href="#">Team feature</a></li>
-          <li><a class="text-muted" href="#">Stuff for developers</a></li>
-          <li><a class="text-muted" href="#">Another one</a></li>
-          <li><a class="text-muted" href="#">Last time</a></li>
-        </ul>
-      </div>
-      <div class="col-6 col-md">
-        <h5>Resources</h5>
-        <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Resource</a></li>
-          <li><a class="text-muted" href="#">Resource name</a></li>
-          <li><a class="text-muted" href="#">Another resource</a></li>
-          <li><a class="text-muted" href="#">Final resource</a></li>
-        </ul>
-      </div>
-      <div class="col-6 col-md">
-        <h5>Resources</h5>
-        <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Business</a></li>
-          <li><a class="text-muted" href="#">Education</a></li>
-          <li><a class="text-muted" href="#">Government</a></li>
-          <li><a class="text-muted" href="#">Gaming</a></li>
-        </ul>
-      </div>
-      <div class="col-6 col-md">
-        <h5>About</h5>
-        <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Team</a></li>
-          <li><a class="text-muted" href="#">Locations</a></li>
-          <li><a class="text-muted" href="#">Privacy</a></li>
-          <li><a class="text-muted" href="#">Terms</a></li>
-        </ul>
-      </div>
-    </div>
-  </footer>
+  <?php
+  include("footer.html");
+  ?>
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
--- recup_donnes.php
+++ recup_donnes.php
@@ -2,17 +2,17 @@
 $user = 'root';
 $pass = '';
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=hackers-poulette', $user, $pass);
-   /* foreach($dbh->query('SELECT * from FOO') as $row) {
+  $dbh = new PDO('mysql:host=localhost;dbname=hackers-poulette', $user, $pass);
+  /* foreach($dbh->query('SELECT * from FOO') as $row) {
         print_r($row);
     }
     $dbh = null;*/
 } catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
+  print "Erreur !: " . $e->getMessage() . "<br/>";
+  die();
 }
-if(isset($_POST['honeypot']) && $_POST['honeypot'] != "")
-die();
+if (isset($_POST['honeypot']) && $_POST['honeypot'] != "")
+  die();
 
 if (isset($_POST['submit'])) {
   if (isset($_POST['name'])) {
@@ -48,35 +48,16 @@ if (isset($_POST['submit'])) {
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <link rel="stylesheet" href="assets/style.css">
   <link rel="icon" type="image/png" href="assets/img/hackers-poulette-logo.png" />
-
   <title>Confirmation</title>
 </head>
 
 <body>
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <a class="navbar-brand">Hackers-poulette</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarCollapse">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Shop</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+  <?php
+  include("header.html");
+  ?>
 
   <main>
     <div class="jumbotron jumbotron-fluid">
@@ -89,52 +70,9 @@ if (isset($_POST['submit'])) {
   </main>
   <hr />
 
-  <footer class="container footer footer-expand-md navbar-dark" id="footer">
-    <div class="row">
-      <div class="col-12 col-md">
-        <img src="assets/img/hackers-poulette-logo.png" width="150" height="150" class="d-block mb-2" role="img" viewBox="0 0 24 24" focusable="false">
-        <title>Product</title></img>
-      </div>
-      <div class="col-6 col-md">
-        <h5>Features</h5>
-        <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Cool stuff</a></li>
-          <li><a class="text-muted" href="#">Random feature</a></li>
-          <li><a class="text-muted" href="#">Team feature</a></li>
-          <li><a class="text-muted" href="#">Stuff for developers</a></li>
-          <li><a class="text-muted" href="#">Another one</a></li>
-          <li><a class="text-muted" href="#">Last time</a></li>
-        </ul>
-      </div>
-      <div class="col-6 col-md">
-        <h5>Resources</h5>
-        <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Resource</a></li>
-          <li><a class="text-muted" href="#">Resource name</a></li>
-          <li><a class="text-muted" href="#">Another resource</a></li>
-          <li><a class="text-muted" href="#">Final resource</a></li>
-        </ul>
-      </div>
-      <div class="col-6 col-md">
-        <h5>Resources</h5>
-        <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Business</a></li>
-          <li><a class="text-muted" href="#">Education</a></li>
-          <li><a class="text-muted" href="#">Government</a></li>
-          <li><a class="text-muted" href="#">Gaming</a></li>
-        </ul>
-      </div>
-      <div class="col-6 col-md">
-        <h5>About</h5>
-        <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Team</a></li>
-          <li><a class="text-muted" href="#">Locations</a></li>
-          <li><a class="text-muted" href="#">Privacy</a></li>
-          <li><a class="text-muted" href="#">Terms</a></li>
-        </ul>
-      </div>
-    </div>
-  </footer>
+  <?php
+  include("footer.html");
+  ?>
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
